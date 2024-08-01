a94876d23837a46bbe6b726155cd724487b35cc8
Andrei Bernatski <iandyone.dev@gmail.com>
2024-08-01 16:58:22 +0300

    feat: add app layout

--- craco.config.ts
+++ craco.config.ts
@@ -5,8 +5,12 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src', 'components'),
+      '@ui': path.resolve(__dirname, 'src', 'components', 'ui'),
       '@constants': path.resolve(__dirname, 'src', 'constants'),
+      '@utils': path.resolve(__dirname, 'src', 'utils'),
+      '@pages': path.resolve(__dirname, 'src', 'pages'),
       '@hooks': path.resolve(__dirname, 'src', 'hooks'),
+      '@assets': path.resolve(__dirname, 'src', 'assets'),
     },
   },
 };
---
+++ src/components/ui/Layout/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+
+import { Footer } from '../Footer';
+
+import { PAGES } from '@constants';
+import { Header } from '@ui/Header';
+
+import styles from './styles.module.css';
+
+export const Layout: FC = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      {pathname === PAGES.NOTES && <Footer />}
+    </div>
+  );
+};
