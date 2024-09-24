ed583465bca472e34e5d599afd7b2dfbed1d0322
iandyone <iandyone.dev@gmail.com>
2024-09-24 17:01:16 +0300

    feat: add form validation

---
+++ src/schemas/sign-up-schema.ts
@@ -0,0 +1,20 @@
+import { FIELD_IS_REQUIRED, getMinLengthErrorMessage } from 'constants/validations';
+
+import { object, string, ref } from 'yup';
+
+export const signUpValidationSchema = object({
+  username: string()
+    .transform((value) => (value ? value.trim() : value))
+    .min(4, getMinLengthErrorMessage(4))
+    .required(FIELD_IS_REQUIRED),
+  email: string()
+    .transform((value) => (value ? value.trim() : value))
+    .min(10, getMinLengthErrorMessage(10))
+    .required(FIELD_IS_REQUIRED),
+  // TODO: обновить длину пароля
+  password: string().min(4, getMinLengthErrorMessage(4)).required(FIELD_IS_REQUIRED),
+  passwordConfirm: string()
+    .min(4, getMinLengthErrorMessage(4))
+    .required(FIELD_IS_REQUIRED)
+    .oneOf([ref('password')], 'Password and confirmation password do not match'),
+});
