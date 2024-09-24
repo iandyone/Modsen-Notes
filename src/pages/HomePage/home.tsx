4a3c2aff1d52784a658e32e0e9d6d2932a7f46d6
Andrei Bernatski <iandyone.dev@gmail.com>
2024-09-24 18:34:06 +0300

    feat: update home page with auth data

--- src/pages/HomePage/home.tsx
+++ src/pages/HomePage/home.tsx
@@ -3,7 +3,8 @@ import { FC, useState } from 'react';
 import welcomeIcon from '@assets/welcome.svg';
 import { SignInForm, SignUpForm } from '@components/Forms';
 import { Heading } from '@components/ui/Heading';
-import { AUTH_MODE } from '@constants';
+import { AUTH_MODE, HOME_WELCOME_MESSAGE, HOME_WELCOME_MESSAGE_WITH_AUTH, HOME_WELCOME_TITLE } from '@constants';
+import { useAuth } from '@hooks';
 
 import styles from './styles.module.css';
 
@@ -11,6 +12,7 @@ const { SIGN_IN, SIGN_UP } = AUTH_MODE;
 
 export const HomePage: FC = () => {
   const [authMode, setAuthMode] = useState(SIGN_IN);
+  const { user } = useAuth();
 
   const handleOnClickAuthButton = () => {
     setAuthMode(authMode === SIGN_IN ? SIGN_UP : SIGN_IN);
@@ -18,20 +20,26 @@ export const HomePage: FC = () => {
 
   return (
     <article className={styles.wrapper}>
-      <Heading title="Welcome to notes" message="Please sign in to your account or create a new" icon={welcomeIcon} />
+      <Heading
+        title={HOME_WELCOME_TITLE}
+        message={!user.id ? HOME_WELCOME_MESSAGE : HOME_WELCOME_MESSAGE_WITH_AUTH}
+        icon={welcomeIcon}
+      />
 
-      <div className={styles.auth}>
-        {authMode === SIGN_IN && <SignInForm className={styles.form} />}
+      {!user.id && (
+        <div className={styles.auth}>
+          {authMode === SIGN_IN && <SignInForm className={styles.form} />}
 
-        {authMode === SIGN_UP && <SignUpForm className={styles.form} />}
+          {authMode === SIGN_UP && <SignUpForm className={styles.form} />}
 
-        <p className={styles.linkBar}>
-          Don't have an account?&nbsp;
-          <span className={styles.authButton} onClick={handleOnClickAuthButton}>
-            {authMode === SIGN_IN ? 'Sign up' : 'Sign in'}
-          </span>
-        </p>
-      </div>
+          <p className={styles.linkBar}>
+            Don't have an account?&nbsp;
+            <span className={styles.authButton} onClick={handleOnClickAuthButton}>
+              {authMode === SIGN_IN ? 'Sign up' : 'Sign in'}
+            </span>
+          </p>
+        </div>
+      )}
     </article>
   );
 };
--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -4,19 +4,18 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import { useNavigate } from 'react-router-dom';
 
 import { Notes } from '@components/Notes';
-import { PAGES, STORAGE_KEYS } from '@constants';
-import { getFromLocalStorage } from '@utils';
+import { PAGES } from '@constants';
+import { useAuth } from '@hooks';
 
 export const NotesPage: FC = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
 
   useLayoutEffect(() => {
-    const accessToken = getFromLocalStorage(STORAGE_KEYS.ACCESS_TOKEN);
-
-    if (!accessToken) {
+    if (!user.id) {
       navigate(PAGES.HOME);
     }
-  }, []);
+  }, [user]);
 
   return (
     <DndProvider backend={HTML5Backend}>
